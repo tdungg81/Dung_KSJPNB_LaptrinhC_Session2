3aa65142be37b42c4ae49d3aa10e3781e08973a3
tdungg81 <dungnguyen8120@gmail.com>
2024-11-20 16:09:43 +0700

    Ex4_Session2

---
+++ Ex4_Session2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int canhhinhvuong = 10; 
+
+    int chuvi = 4 * canhhinhvuong; 
+    int dientich = canhhinhvuong * canhhinhvuong; 
+
+    printf("Canh hinh vuong la: %d\n", canhhinhvuong);
+    printf("Chu vi hinh vuong la: %d\n", chuvi);
+    printf("Dien tich hinh vuong la: %d\n", dientich);
+
+    return 0; 
+}
+
