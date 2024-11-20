e13f032917b9c9552fd407d44497df69484df5aa
tdungg81 <dungnguyen8120@gmail.com>
2024-11-20 16:10:09 +0700

    Ex5_Session2

---
+++ Ex5_Session2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+	int chieudai = 10;
+	int chieurong = 5;
+	
+	int chuvihinhchunhat = 2 * (chieudai + chieurong);
+	int dientichhinhchunhat = chieudai * chieurong;
+	
+	printf("Chu vi hinh chu nhat la: %d\n", chuvihinhchunhat);
+	printf("Dien tich hinh chu nhat la: %d\n", dientichhinhchunhat);
+	
+	return 0; 
+}
